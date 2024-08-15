474965d07c51f2c88b9ec472d73f08a17ebfa137
ShikharSingh-GitHub <shikharsingh024@gmail.com>
2024-08-15 22:36:04 +0530

    added routes and react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 // src/App.jsx
 
 import React from 'react';
+import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -10,8 +11,7 @@ import Hobbies from './components/Hobbies';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
 import Skills from './components/Skills';
-
-const App = () => {
+/*const App = () => {
   return (
     <div>
       <Header />
@@ -24,6 +24,23 @@ const App = () => {
       <Resume />
     </div>
   );
-};
+};*/
+
+function App() {
+  return (
+    <Router>
+      <Header /> {/* Header will be displayed on all pages */}
+      <Routes>
+        <Route path="/" element={<About />} /> {/* Default route */}
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/hobbies" element={<Hobbies />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
