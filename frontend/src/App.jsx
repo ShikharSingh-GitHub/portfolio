03bbcea36abe18c98257cbaf9a9a102b490a708c
ShikharSingh-GitHub <shikharsingh024@gmail.com>
2024-08-15 23:21:58 +0530

    resetting todays work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 // src/App.jsx
 
 import React from 'react';
-import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -11,7 +10,8 @@ import Hobbies from './components/Hobbies';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
 import Skills from './components/Skills';
-/*const App = () => {
+
+const App = () => {
   return (
     <div>
       <Header />
@@ -24,23 +24,6 @@ import Skills from './components/Skills';
       <Resume />
     </div>
   );
-};*/
-
-function App() {
-  return (
-    <Router>
-      <Header /> {/* Header will be displayed on all pages */}
-      <Routes>
-        <Route path="/" element={<About />} /> {/* Default route */}
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/hobbies" element={<Hobbies />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
-  );
-}
+};
 
 export default App;
