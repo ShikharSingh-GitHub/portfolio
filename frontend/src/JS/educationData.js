ac92a7cc8f3249610f008717ae8acf818d8fb1bd
ShikharSingh-GitHub <shikharsingh024@gmail.com>
2024-10-08 22:01:37 +0530

    Changed an Education Card

--- frontend/src/JS/educationData.js
+++ frontend/src/JS/educationData.js
@@ -1,43 +1,48 @@
 // src/educationData.js
 
 const education = [
-    {
-        course: 'Software Development, Product Management and Data Engineering',
-        institution: 'Scaler Technology',
-        year: '2022 - 2024',
-        description: 'Focused on software development, data structures, and algorithms. Gained experience in full-stack development and participated in various technical projects.',
-      },
-      {
-        course: 'Union Public Service Commission, Civil Services Exam(UPSC CSE) Aspirant',
-        institution: 'Vajiram and Ravi, New Delhi',
-        year: '2018 - 2022',
-        description: "Learnt about the Social Sciences like History, Polity, Geography, Art and Culture, Geopolitics, Ethics and Morality, Sociology, Current Affairs while preparing for one of the world's toughest Exams.",
-      },
-    {
-      course: 'Bachelor of Engineering in Information Technology',
-      institution: 'Medicaps Institute of Science and Technology, Indore',
-      year: '2013 - 2017',
-      description: 'Focused on software development, data structures, and algorithms. Gained experience in full-stack development and participated in various technical projects and hackathons.',
-    },
-    {
-      course: 'Higher Secondary Education (12th Grade)',
-      institution: 'Kendriya Vidyalaya No. 1, Indore',
-      year: '2012 - 2013',
-      description: 'Specialized in Mathematics, Physics, Chemistry and Computer Science. Developed a strong foundation in logical reasoning and problem-solving skills.',
-    },
-    {
-      course: 'Secondary Education (10th Grade)',
-      institution: 'Kendriya Vidyalaya No. 1, Indore',
-      year: '2010 - 2011',
-      description: 'Completed secondary education with a focus on foundational subjects including Mathematics and Science.',
-    },
-    {
-      course: 'Primary Education',
-      institution: 'Kendriya Vidyalaya No. 1, Indore',
-      year: '2003 - 2010',
-      description: 'Completed Primary education with a Lot of Co-Curricular Activities Such as Cubs and Bulbul, Sports, Music, Arts, Science Quizzes, General knowledge Quizzes etc.',
-    },
-  ];
-  
-  export default education;
-  
+  {
+    course: "Software Development, Product Management and Data Engineering",
+    institution: "Scaler Technology",
+    year: "2022 - 2024",
+    description:
+      "Focused on software development, data structures, and algorithms. Gained experience in full-stack development and participated in various technical projects.",
+  },
+  {
+    course: "Freelance Developer",
+    institution: "Self-employed",
+    year: "2018 - 2022",
+    description:
+      "Provided freelancing services, focusing on web development and design. Created dynamic frontends, optimized backend operations, and collaborated with clients on various projects, ensuring timely deliveries and satisfaction.",
+  },
+  {
+    course: "Bachelor of Engineering in Information Technology",
+    institution: "Medicaps Institute of Science and Technology, Indore",
+    year: "2013 - 2017",
+    description:
+      "Focused on software development, data structures, and algorithms. Gained experience in full-stack development and participated in various technical projects and hackathons.",
+  },
+  {
+    course: "Higher Secondary Education (12th Grade)",
+    institution: "Kendriya Vidyalaya No. 1, Indore",
+    year: "2012 - 2013",
+    description:
+      "Specialized in Mathematics, Physics, Chemistry and Computer Science. Developed a strong foundation in logical reasoning and problem-solving skills.",
+  },
+  {
+    course: "Secondary Education (10th Grade)",
+    institution: "Kendriya Vidyalaya No. 1, Indore",
+    year: "2010 - 2011",
+    description:
+      "Completed secondary education with a focus on foundational subjects including Mathematics and Science.",
+  },
+  {
+    course: "Primary Education",
+    institution: "Kendriya Vidyalaya No. 1, Indore",
+    year: "2003 - 2010",
+    description:
+      "Completed Primary education with a Lot of Co-Curricular Activities Such as Cubs and Bulbul, Sports, Music, Arts, Science Quizzes, General knowledge Quizzes etc.",
+  },
+];
+
+export default education;
